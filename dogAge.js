f70833c346a2b0c7566107ba99071c9ceae6d66c
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-09-23 14:52:23 +0300

    dog age exercise

---
+++ dogAge.js
@@ -0,0 +1,12 @@
+const myAge = 31;
+const myName = "Viktoriia";
+
+//this is my first 2 yaers
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//i need to calsulate the rest
+const restYears = (myAge - 2) * 4;
+const sum = earlyYears + restYears;
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${sum} years old in dog years.`);
