e05e9829680cd3b6b3e54d58c156b14efe95c4de
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-10-06 11:46:02 +0300

    add methods

--- iterationMethods.js
+++ iterationMethods.js
@@ -44,3 +44,39 @@ const longFavoriteWords = favoriteWords.filter(word => {
   return word.length > 7;
 })
 console.log(longFavoriteWords);
+
+//.findIndex
+const animals2 = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+
+const foundAnimal = animals2.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+const startsWithS = animals2.findIndex(animal=> {
+  return animal[0] === 's';
+})
+
+////////ALL METHODS/////
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
